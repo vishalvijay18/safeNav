a4a3feaebad44bd1b3318a49ecdc0162c1474c72
chandan mishra <ckmishra01@gmail.com>
2016-02-27 19:27:47 -0800

    second Commit

--- app/src/main/java/nwhacks/com/safenav/Home.java
+++ app/src/main/java/nwhacks/com/safenav/Home.java
@@ -1,19 +1,115 @@
 package nwhacks.com.safenav;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Home extends ActionBarActivity {
+     double latitude;
+     double longitute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+           final EditText origin =(EditText)findViewById(R.id.origin);
+           final EditText destination =(EditText)findViewById(R.id.dest);
+           final Button go =(Button) findViewById(R.id.go);
+           go.setOnClickListener(new Button.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    try {
+
+                        String org = origin.getText().toString();
+                        org.replace(" ", "+");
+                        String dest = destination.getText().toString();
+                        dest.replace(" ", "+");
+                        //Intent geo = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + org));
+                        //startActivity(geo);
+                        getLocationInfo(org);
+                        getLocationInfo(dest);
+                    }catch(Exception e){
+                        //Log.e(TAG, e.getMessage());
+                    }
+                }
+
+        });
     }
 
+    public  JSONObject getLocationInfo(String address) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+
+            address = address.replaceAll(" ","%20");
+
+            HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address + "&sensor=false");
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response;
+            stringBuilder = new StringBuilder();
+
+            response = client.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream stream = entity.getContent();
+            int b;
+            while ((b = stream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+
+    public  boolean getLatLong(JSONObject jsonObject) {
+
+        try {
+            longitute = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            latitude = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+        } catch (JSONException e) {
+            return false;
+
+        }
+
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
